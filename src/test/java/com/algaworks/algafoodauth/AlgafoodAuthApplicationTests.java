c2d02cc9b544bc06f935cfc81580aa8cfdb9464a
wilian.souza <wilian.souza@dtidigital.com.br>
2023-12-27 15:17:11 -0300

    Implemented authorization server with client credentials.

---
+++ src/test/java/com/algaworks/algafoodauth/AlgafoodAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.algaworks.algafoodauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgafoodAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
