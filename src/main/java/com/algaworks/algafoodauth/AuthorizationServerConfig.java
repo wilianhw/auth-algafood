daa2c50ef359201d1be99c15ba76974dcad6b947
wilian.souza <wilian.souza@dtidigital.com.br>
2024-01-02 22:04:56 -0300

    Implemented authorization server with grant type code.

--- src/main/java/com/algaworks/algafoodauth/AuthorizationServerConfig.java
+++ src/main/java/com/algaworks/algafoodauth/AuthorizationServerConfig.java
@@ -1,5 +1,7 @@
 package com.algaworks.algafoodauth;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
 import java.io.InputStream;
 import java.security.KeyStore;
 import java.time.Duration;
@@ -9,7 +11,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.core.io.Resource;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.core.AuthorizationGrantType;
@@ -19,8 +26,10 @@ import org.springframework.security.oauth2.server.authorization.client.Registere
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
 import org.springframework.security.oauth2.server.authorization.settings.AuthorizationServerSettings;
+import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
 import org.springframework.security.oauth2.server.authorization.settings.OAuth2TokenFormat;
 import org.springframework.security.oauth2.server.authorization.settings.TokenSettings;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import com.nimbusds.jose.jwk.JWKSet;
@@ -36,8 +45,33 @@ public class AuthorizationServerConfig {
     @Order(Ordered.HIGHEST_PRECEDENCE)
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(http);
-        return http.build();
+        return http.formLogin(withDefaults()).build();
     }
+
+    @Bean
+    @Order(2)
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http, UserDetailsService userDetailsService) throws Exception {
+        http
+                .userDetailsService(userDetailsService)
+                .cors(Customizer.withDefaults())
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorizeHttp -> authorizeHttp
+                        .anyRequest().authenticated());
+        return http.formLogin(withDefaults()).build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails userDetails = User
+                .withUsername("wilian")
+                .password(passwordEncoder().encode("1234"))
+                .authorities("READ", "WRITE")
+                .roles("READ")
+                .build();
+
+        return new InMemoryUserDetailsManager(userDetails);
+    }
+
 
     @Bean
     public AuthorizationServerSettings providerSettings(AlgafoodSecurityProperties properties) {
@@ -48,8 +82,8 @@ public class AuthorizationServerConfig {
 
     @Bean
     public RegisteredClientRepository users() {
-        RegisteredClient registeredClient = RegisteredClient.withId("1")
-                .clientId("algafood-web")
+        RegisteredClient registeredClient1 = RegisteredClient.withId("1")
+                .clientId("algafood-web1")
                 .clientSecret(passwordEncoder().encode("1234"))
                 .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
                 .authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
@@ -60,7 +94,41 @@ public class AuthorizationServerConfig {
                         .build())
                 .build();
 
-        return new InMemoryRegisteredClientRepository(registeredClient);
+        RegisteredClient registeredClient2 = RegisteredClient.withId("2")
+                .clientId("algafood-web2")
+                .clientSecret(passwordEncoder().encode("1234"))
+                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+                .scope("READ")
+                .scope("WRITE")
+                .tokenSettings(TokenSettings.builder()
+                        .accessTokenFormat(OAuth2TokenFormat.SELF_CONTAINED)
+                        .accessTokenTimeToLive(Duration.ofMinutes(15))
+                        .build())
+                .redirectUri("http://127.0.0.1:8082/teste")
+                .clientSettings(ClientSettings.builder()
+                        .requireAuthorizationConsent(true)
+                        .build())
+                .build();
+
+        RegisteredClient registeredClient3 = RegisteredClient.withId("3")
+                .clientId("algafood-web3")
+                .clientSecret(passwordEncoder().encode("1234"))
+                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+                .scope("READ")
+                .scope("WRITE")
+                .tokenSettings(TokenSettings.builder()
+                        .accessTokenFormat(OAuth2TokenFormat.SELF_CONTAINED)
+                        .accessTokenTimeToLive(Duration.ofMinutes(30))
+                        .build())
+                .redirectUri("http://127.0.0.1:8082/authorized")
+                .clientSettings(ClientSettings.builder()
+                        .requireAuthorizationConsent(false)
+                        .build())
+                .build();
+
+        return new InMemoryRegisteredClientRepository(registeredClient1, registeredClient2, registeredClient3);
     }
 
     @Bean
